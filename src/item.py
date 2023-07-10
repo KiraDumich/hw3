beba4d7a02ff6f3084301d3dc7abb7d1b7f4ab71
79244 <dumichkira05@gmail.com>
2023-07-10 12:42:48 +1000

    10.07

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,26 +7,48 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+    def __init__(self, name, one_pay, count):
+        self.__name = name
+        self.one_pay = one_pay
+        self.count = count
+        Item.price = one_pay
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
-        pass
+        total_price = self.one_pay * self.count
+        return total_price
 
     def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+        Item.price = self.one_pay * Item.pay_rate
+
+    @property
+    def name(self):
+        return f'{self.__name}'
+
+    @name.setter
+    def name(self, item):
+        if len(item) > 10:
+            self.__name == item[:10]
+        else:
+            self.__name = item
+
+    @staticmethod
+    def string_to_number(item):
+        if item.isdigit():
+            return int(item)
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', 'r', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for item in reader:
+                print(f"{item['name']}, {item['price']}, {item['quantity']}")
+                name = item['name']
+                price = cls.string_to_number(item['price'])
+                quantity = cls.string_to_number(item['quantity'])
+                items = cls(name, price, quantity)
+                cls.all.append(items)
+                print(len(cls.all))
+
+
+
